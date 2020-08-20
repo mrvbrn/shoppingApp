1df73fcb101734b9b07d1a64dbbfc0ae9b3725a5
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-08-20 11:59:16 -0700

    created products overview screen

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import productsReducer from "./store/reducers/product";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 export default function App() {
+  const rootReducer = combineReducers({
+    products:productsReducer
+  });
+
+  const store = createStore(rootReducer);
+
   return (
-    <View style={styles.container}>
-      <Text>hello</Text>   
-    </View>
+    <Provider>
+      <View>
+      </View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { createStore, combineReducer } from "redux";
+import { Provider } from "react-redux";
+import { PRODUCTS } from "../data/dummy-data";
+
+const ProductOverviewScreen = props => {
+    <View>
+
+    </View>
+}
+
+
+const styles=StyleSheet.create({
+
+})
+
+export default ProductOverviewScreen;
