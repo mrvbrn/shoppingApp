f844f0b8aaf4eb0503a8f60bfc7de4a9b7c144aa
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-08-22 13:43:39 -0700

    worked product details page

--- screens/shop/ProductDetail.js
+++ screens/shop/ProductDetail.js
@@ -1,15 +1,21 @@
 import React from "react";
-import {View, Button, Text, StyleSheet } from "react-native";
+import {View, Button, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { useSelector } from "react-redux";
+import Colors from "../../constants/Colors";
 
 
 const ProductDetail = props => {
     const productId = props.navigation.getParam('productId')
     const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId));
   return(
-    <View> 
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView> 
+      <Image style={styles.image} source={{uri:selectedProduct.imageUrl}}/>
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to Cart"/>
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>   
+    </ScrollView>
   )
 }
 
@@ -20,6 +26,25 @@ ProductDetail.navigationOptions = navData => {
 }
 
 const styles=StyleSheet.create({
+    image:{
+        width:'100%',
+        height:300,
+    },
+    price:{
+        fontSize:20,
+        color:'#888',
+        marginVertical:10,
+        textAlign:'center'
+    },
+    description:{
+        fontSize:14,
+        textAlign:'center',
+        marginHorizontal:20
+    },
+    actions:{
+        marginVertical:10,
+        alignItems:'center'
+    }
 
 })
 
