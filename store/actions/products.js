1cfbc8ff795ace32cdb2723d24e22d443a719821
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-09-19 13:51:56 -0700

    handling errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -43,9 +43,12 @@ export const fetchProducts = () => {
 
 export const deleteProduct = (productId) => {
     return  async dispatch => {
-      await fetch (`https://shopping-app-846d8.firebaseio.com/products/${productId}.json`, {
+      const response = await fetch (`https://shopping-app-846d8.firebaseio.com/products/${productId}.json`, {
         method:'DELETE',
       });
+    if(response.ok){
+        throw new Error('Something went wrong');
+    }
 
     dispatch ({ type: DELETE_PRODUCT, pid:productId});
     };
@@ -89,7 +92,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async dispatch => {
-    await fetch(
+    const response = await fetch(
       `https://shopping-app-846d8.firebaseio.com/products/${id}.json`,
       {
         method: 'PATCH',
@@ -103,6 +106,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         })
       }
     );
+
+    if(!response.ok){
+        throw new Error('Something went wrong')
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
