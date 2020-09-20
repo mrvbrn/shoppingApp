e53bf4c563e41a8cfc921ff679a83922bdaa39b9
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-09-19 22:49:24 -0700

    fetching stored orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,40 @@
+import Order from "../../models/order";
+
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDER = 'SET_ORDER';
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch(
+        "https://shopping-app-846d8.firebaseio.com/orders/u1.json"
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order (
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )   
+        );
+      }
+
+      dispatch({ type: SET_ORDER, orders: loadedOrders});
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
