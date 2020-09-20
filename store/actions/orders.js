4fa08c7e624189f9cc4717337e346091e1fee44b
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-09-19 22:03:21 -0700

    stored order

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,29 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
+  return async dispatch => {
+    const date =  new Date()
+    const response = await fetch('https://shopping-app-846d8.firebaseio.com/orders/u1.json',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        cartItems,
+        totalAmount,
+        date : date.toISOString()
+      })
+    }
+  );
+  if (!response.ok){
+    throw new Error ('Something went wrong');
+  }
+
+  const resData = await response.json();
+  dispatch ({
     type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+    orderData: { id: resData.name, items: cartItems, amount: totalAmount, date:date }
+  });
   };
 };
