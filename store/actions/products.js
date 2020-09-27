97a3854aa053732a27b05ac878650b9b0240fcbd
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-09-26 18:29:31 -0700

    mapping orders to users

--- store/actions/products.js
+++ store/actions/products.js
@@ -6,7 +6,8 @@ export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const userId =  getState().auth.userId;
     try {
       const response = await fetch(
         "https://shopping-app-846d8.firebaseio.com/products.json"
@@ -32,7 +33,7 @@ export const fetchProducts = () => {
         );
       }
 
-      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts, userProducts:loadedProducts.filter(prod => prod.ownerId === userId)});
     } catch (err) {
       throw err;
     }
@@ -56,8 +57,9 @@ export const deleteProduct = (productId) => {
 
 export const createProduct = (title, description, imageUrl, price) => {
   return async (dispatch, getState) => {
-    const token = getState().auth.token
-    const (response) = await fetch(
+    const token = getState().auth.token;
+    const userId = getState().auth.userId;
+    const response = await fetch(
       `https://shopping-app-846d8.firebaseio.com/products.json?auth=${token}`,
       {
         method: 'POST',
@@ -68,7 +70,8 @@ export const createProduct = (title, description, imageUrl, price) => {
           title,
           description,
           imageUrl,
-          price
+          price,
+          ownerId:userId
         })
       }
     );
@@ -82,7 +85,8 @@ export const createProduct = (title, description, imageUrl, price) => {
         title,
         description,
         imageUrl,
-        price
+        price,
+        ownerId:userId
       }
     });
   };
